d6c8118b9f8a43218070966b573de6e2cf8ab26e
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-22 16:11:58 +0200

    updated

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -18,7 +18,9 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    director: PropTypes.string
+    director: PropTypes.string,
+    genre: PropTypes.shape({
+    })
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
