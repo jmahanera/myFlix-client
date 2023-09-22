4616f0c04a1534f42d9b10a503efc56ca1a6c24b
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-22 12:39:48 +0200

    code updated

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -16,7 +16,9 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 // Here is where we define all the props constraints for the BookCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.string
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
