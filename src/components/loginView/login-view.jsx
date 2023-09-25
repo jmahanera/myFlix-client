ae18702f4d8f67f86dd545515022516f8a890c1e
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-25 12:33:33 +0200

    ud

--- src/components/loginView/login-view.jsx
+++ src/components/loginView/login-view.jsx
@@ -1,51 +1,49 @@
 import React, { useState } from "react";
-import { jsx as _jsx } from 'react/jsx-runtime';
 
 export const LoginView = () => {
-    const [username, setUsername] = useState("");  // Declare state for username
-    const [password, setPassword] = useState("");  // Declare state for password
+  const [username, setUsername] = useState("");  // Declare state for username
+  const [password, setPassword] = useState("");  // Declare state for password
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        const data = {
-            access: username,
-            secret: password
-        };
+    const data = {
+      access: username,
+      secret: password
+    };
 
-        fetch("https://openlibrary.org/account/login.json", {
-            method: "POST",
-            body: JSON.stringify(data)
-        }).then((response) => {
-            if (response.ok) {
-                onLoggedIn(username);
-            } else {
-                alert("Login failed");
-            }
-        });
+    fetch("https://primemovies-39075872fbeb.herokuapp.com/account/login", {
+   method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
 
-        return (
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password:
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
-        );
-    }
-};
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+  type="password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+/>
+      </label>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}};
