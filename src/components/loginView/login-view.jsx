168d2525392a881a82bde4dfebc300936ca352c3
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-25 14:49:02 +0200

    update

--- src/components/loginView/login-view.jsx
+++ src/components/loginView/login-view.jsx
@@ -12,7 +12,7 @@ export const LoginView = () => {
       secret: password
     };
 
-    fetch("https://primemovies-39075872fbeb.herokuapp.com/account/login.json", {
+    fetch("https://primemovies-39075872fbeb.herokuapp.com/login.json", {
       method: "POST",
       body: JSON.stringify(data)
     });
