f7dc236b639377694452c48017372cbf52f82f23
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-26 11:13:44 +0200

    update

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -25,6 +25,9 @@ export const MainView = () => {
     });
   }, []);
   
+
+
+
   if (selectedMovie) {
     return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
   }
--- src/dist/index.0d6fcb10.js
+++ src/dist/index.0d6fcb10.js
@@ -27196,14 +27196,14 @@ const MainView = ()=>{
         onBackClick: ()=>setSelectedMovie(null)
     }, void 0, false, {
         fileName: "src/components/MainView/main-view.jsx",
-        lineNumber: 29,
+        lineNumber: 32,
         columnNumber: 12
     }, undefined);
     if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: "The Movie list is empty!"
     }, void 0, false, {
         fileName: "src/components/MainView/main-view.jsx",
-        lineNumber: 33,
+        lineNumber: 36,
         columnNumber: 12
     }, undefined);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -27212,12 +27212,12 @@ const MainView = ()=>{
                 onMovieClick: (movie)=>setSelectedMovie(movie)
             }, movie._id, false, {
                 fileName: "src/components/MainView/main-view.jsx",
-                lineNumber: 39,
+                lineNumber: 42,
                 columnNumber: 9
             }, undefined))
     }, void 0, false, {
         fileName: "src/components/MainView/main-view.jsx",
-        lineNumber: 37,
+        lineNumber: 40,
         columnNumber: 5
     }, undefined);
 };
@@ -27253,7 +27253,7 @@ const MovieCard = ({ movie, onMovieClick })=>{
     const [isClicked, setIsClicked] = (0, _react.useState)(false);
     const handleClick = ()=>{
         setIsClicked(!isClicked);
-        onMovieClick(movie);
+        onMovieClick(movie.image);
     };
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         onClick: handleClick,
