409c607af9da9ddaa5bd81a9e56d3f457ce58a49
jula <122871245+jmahanera@users.noreply.github.com>
2023-10-09 12:27:30 +0200

    Update movie-view.jsx

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,56 +1,34 @@
-import React, { useEffect } from "react";
-import './movie-view.scss';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./movie-view.scss";
 
-export const MovieView = ({ movie, onBackClick, movieViewRef }) => {
-  useEffect(() => {
-    // Scroll to the top of the movie view
-    if (movieViewRef.current) {
-      movieViewRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-  }, [movieViewRef]);
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+
+  // Find the specific movie by movieId
+  const movie = movies.find((m) => m.id === movieId);
+
+  if (!movie) {
+    return <div>No movie found.</div>;
+  }
 
   return (
-    <div ref={movieViewRef}>
-      <div key={movie.id}>
-        <div>
-          <img className="w-100" src={movie.imageUrl} alt={movie.title} />
-        </div>
-        <div>
-          <span className="movie-title">Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span className="movie-description">Description: </span>
-          <span>{movie.description}</span>
-        </div>
-        <div>
-          <span className="movie-genre">Genre: </span>
-          <span>{movie.genre && movie.genre.name}</span>
-        </div>
-        <div>
-          <span className="movie-director">Director: </span>
-          <span>{movie.director && movie.director.name}</span>
-        </div>
-        <div>
-          <span className="movie-biography">Biography: </span>
-          <span>{movie.director && movie.director.director_bio}</span>
-        </div>
-        <div>
-          <span className="movie-birthyear">Birthyear: </span>
-          <span>{movie.director && movie.director.birthyear}</span>
-        </div>
-        <div>
-          <span className="movie-actors">Actors: </span>
-          <span>{movie.actors && movie.actors.join(', ')}</span>
-        </div>
-        {/* Add other movie details you want to display */}
+    <div>
+      <div>
+        <img className="w-100" src={movie.imageUrl} alt={movie.title} />
       </div>
-      <button
-        onClick={onBackClick}
-        className="back-button"
-        style={{ cursor: "pointer" }}>
-        Back
-      </button>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
     </div>
   );
 };
