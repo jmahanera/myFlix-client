39a75d603966c3e025823b3bd65e918a103ddab4
jula <122871245+jmahanera@users.noreply.github.com>
2023-10-12 13:14:26 +0200

    Update movie-view.jsx

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,44 +1,34 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
-
-  console.log("Received movies prop:", movies);
-  console.log("Received movieId:", movieId);
-
-  if (!movieId) {
-    console.log("No movie ID provided.");
-    return <div>No movie ID provided.</div>;
-  }
-
-  // Find the specific movie by movieId
-  const movie = movies.find((m) => m.id === movieId);
-
-  if (!movie) {
-    console.log("No movie found with ID:", movieId);
-    return <div>No movie found.</div>;
-  }
-
-  console.log("Selected movie:", movie);
+  const movie = movies.find((movie) => movie.id === movieId);
 
   return (
     <div>
-      <div>
-        <img className="w-100" src={movie.imageUrl} alt={movie.title} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
+      {movie ? (
+        <div>
+          <div>
+            <img className="w-100" src={movie.imageUrl} alt={movie.title} />
+          </div>
+          <div>
+            <span>Title: </span>
+            <span>{movie.title}</span>
+          </div>
+          <div>
+            <span>Description: </span>
+            <span>{movie.description}</span>
+          </div>
+        </div>
+      ) : (
+        <div>No movie found.</div>
+      )}
+
+      {/* Always provide a "Back" button */}
+      <Link to="/movies">
+        <button className="btn btn-primary">Back</button>
       </Link>
     </div>
   );
