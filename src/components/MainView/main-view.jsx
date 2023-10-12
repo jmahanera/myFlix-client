31a9206fc757ae984afc12a2c03cc33d49d66008
jula <122871245+jmahanera@users.noreply.github.com>
2023-10-12 13:13:52 +0200

    Update main-view.jsx

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,46 +1,48 @@
-import { NavigationBar } from "../Navigation-bar/navigation-bar"; // Update the import path
 import React, { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
-import { MovieView } from "../MovieView/movie-view";
+import { NavigationBar } from "../Navigation-bar/navigation-bar";
 import { LoginView } from "../loginView/login-view";
 import { SignupView } from "../signupView/sign-up-view";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { MovieView } from "../MovieView/movie-view";
 
 export const MainView = () => {
-  const storedToken = localStorage.getItem("token"); // Retrieve the token from localStorage
-  const storedUser = localStorage.getItem("user"); // Retrieve the user from localStorage
-  const [token, setToken] = useState(storedToken || null); // Initialize token state with stored token
-  const [user, setUser] = useState(JSON.parse(storedUser) || null); // Initialize user state with stored user
+  const storedToken = localStorage.getItem("token");
+  const storedUser = localStorage.getItem("user");
+  const [token, setToken] = useState(storedToken || null);
+  const [user, setUser] = useState(JSON.parse(storedUser) || null);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    if (!token) return; // If there is no token, don't bother
+    if (!token) return;
 
     fetch("https://primemovies-39075872fbeb.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }, // Send the token in the Authorization header
+      headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => response.json())
       .then((movies) => {
         const moviesFromApi = movies?.map((movie) => ({
-          id: movie._id, // The ID in the API is called "_id"
+          id: movie._id,
           title: movie.title,
-          imageUrl: movie.imageUrl, // fixed typo
+          imageUrl: movie.imageUrl,
           description: movie.description,
         }));
         setMovies(moviesFromApi);
       });
-  }, [setMovies]); // Don't add movies to dependencies, as it would cause an infinite loop
+  }, [token]);
 
   return (
     <BrowserRouter>
-      <NavigationBar user={user} onLoggedOut={() => {
-        setUser(null);
-        setToken(null); // removes token from localStorage on logout
-        localStorage.clear(); // clears localStorage on logout
-      }} />{" "}
-      {/* Step 5 */}
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      />
       <Row className="justify-content-md-center">
         <Routes>
           <Route
@@ -58,26 +60,26 @@ export const MainView = () => {
             }
           />
           <Route
-  path="/"
-  element={
-    <>
-      {!user ? (
-        <Navigate to="/login" replace />
-      ) : movies.length === 0 ? (
-        <Col>The list is empty!</Col>
-      ) : (
-        <>
-          {movies.map((movie) => (
-            <Col className="mb-4" key={movie.id} md={3}>
-              <MovieCard movie={movie} cardClassName="movie-card" />
-            </Col>
-          ))}
-        </>
-      )}
-    </>
-  }
-/>
-
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView
+                      onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                        localStorage.setItem("token", token);
+                        localStorage.setItem("user", JSON.stringify(user));
+                      }}
+                    />
+                  </Col>
+                )}
+              </>
+            }
+          />
           <Route
             path="/movies"
             element={
@@ -95,6 +97,14 @@ export const MainView = () => {
             }
           />
           <Route
+            path="/movies/:movieId"
+            element={
+              <Col md={8}>
+                <MovieView movies={movies} />
+              </Col>
+            }
+          />
+          <Route
             path="/"
             element={
               <>
@@ -105,7 +115,7 @@ export const MainView = () => {
                 ) : (
                   <>
                     {movies.map((movie) => (
-                      <Col className="mb-4" key={movie.id} md={3}>
+                      <Col className="mb-4" key={movie._id} md={3}>
                         <MovieCard movie={movie} />
                       </Col>
                     ))}
